a2bf8164d4f6c78da07d8dd3bfc1fd515a74e88a
hmy <hmyhmy>
2023-07-27 14:33:50 +0900

    hw

--- 07-27/hw02.py
+++ 07-27/hw02.py
@@ -5,26 +5,22 @@ class UserInfo:
     def __init__(self):
         self.user_data = {}
     
+    
     def get_user_info(self):
+        
         while 1:
             try:
                 self.user_data["name"] = input("이름을 입력하세요 :")
-                if self.user_data["name"] == "끝":
-                    break
                 self.user_data["age"] = int(input("나이를 입력하세요 :"))
-                if self.user_data["age"] == "끝":
-                    break
-                self.user_data["data"] = input("사용자 정보 : ")
-                if self.user_data["data"] == "":
-                    print("사용자 정보가 입력되지 않았습니다.")
-                if self.user_data["data"] == "끝":
-                    break
+                print("사용자 정보 : ")
+                print(f"이름 : {self.user_data['name']}")
+                print(f"나이 : {self.user_data['age']}")
             except ValueError:
-                print("나이는 숫자로 입력해야 합니다")
-
+                break
+    
     def display_user_info(self):
-        print(self.user_data)
-        pass
+        print("나이는 숫자로 입력해야 합니다.")    
+        print("사용자 정보가 입력되지 않았습니다.")
 
 user = UserInfo()
 user.get_user_info()
