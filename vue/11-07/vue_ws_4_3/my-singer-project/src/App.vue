dfbba3c9bf9316dc4f4338657a8fe80844dc9903
이정민 <bona3377@naver.com>
2023-11-07 12:31:43 +0900

    ws

---
+++ vue/11-07/vue_ws_4_3/my-singer-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MainPage/>
+    <singerList/>
+    <div class="d-flex justify-content-center">
+      <SingerDetail/>
+      <SingerDetail/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import MainPage from '@/components/MainPage.vue';
+  import SingerDetail from '@/components/singer/SingerDetail.vue';
+  import singerList from '@/components/singerList.vue';
+</script>
+
+<style scoped>
+
+</style>
